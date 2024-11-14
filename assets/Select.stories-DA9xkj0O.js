2442b52062c231105dac66d482e6a1895078de1c
Lterwingen <lukas.terwingen97@gmail.com>
2024-11-14 14:54:59 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@28c3dd262e7b529e3e4dda6015283c36c9ec7671 🚀

---
+++ assets/Select.stories-DA9xkj0O.js
@@ -0,0 +1,32 @@
+import{S as d}from"./Select-B0F8vQcl.js";import"./jsx-runtime-Cw0GR0a5.js";import"./index-CTjT7uj6.js";import"./index-DEHCCq9W.js";import"./SVpyu8SL-kb1JahyB.js";import"./KeyboardArrowDown-BIpZZpcG.js";import"./Info-Dq1_dlTC.js";import"./index-Bb4qSo10.js";import"./Button-CoFn26zJ.js";import"./useFocusRing-COlWfXo5.js";import"./index-9r8iugjR.js";import"./Hidden-BsqWnUMT.js";import"./useFocusable-THzxVUx6.js";import"./usePress-txE9COHB.js";import"./FieldError-mOHOEFEk.js";import"./Text-DmzI781W.js";import"./useLabels--xxESjfg.js";import"./Form-_FoLAR_T.js";import"./intlStrings-ZMTSFH_M.js";import"./ListBox-CO8fSrKd.js";import"./Collection-CA0OIEZf.js";import"./DragAndDrop-Msm58r93.js";import"./context-Ao51U0H-.js";import"./FocusScope-DkCrxc-x.js";import"./SelectionManager-B2MAkms-.js";import"./useControlledState-CEg4Rl-x.js";import"./useListState-CgnqO5Wd.js";import"./Dialog-C3i89GLb.js";import"./RSPContexts-CHzP-Sy3.js";import"./PortalProvider-DrflOcEs.js";import"./ariaHideOutside-CWagKFk4.js";import"./useLocalizedStringFormatter-CfLVTgOs.js";import"./VisuallyHidden-D6MjpNXd.js";import"./useSingleSelectListState-WiA2AzUx.js";import"./useMenuTrigger-DX8xHMzw.js";import"./useFormReset-Dk63eIg3.js";import"./ListBox-B0oGONat.js";import"./Check-C4fwrEtC.js";import"./Popover-Jhy2Mjt3.js";const Z={component:d},r={args:{placeholder:"Select an item",items:[{id:"1",label:"Backlog"},{id:"2",label:"In Progress"},{id:"3",label:"In Review"},{id:"4",label:"Done"},{id:"5",label:"Won't Do"}]}},e={args:{...r.args,label:"Status",description:"Select a status"}},t={args:{...e.args,isDisabled:!0}};var o,i,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    placeholder: "Select an item",
+    items: [{
+      id: "1",
+      label: "Backlog"
+    }, {
+      id: "2",
+      label: "In Progress"
+    }, {
+      id: "3",
+      label: "In Review"
+    }, {
+      id: "4",
+      label: "Done"
+    }, {
+      id: "5",
+      label: "Won't Do"
+    }]
+  }
+}`,...(a=(i=r.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};var s,p,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    label: "Status",
+    description: "Select a status"
+  }
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var l,n,c;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...WithLabelAndDescription.args,
+    isDisabled: true
+  }
+}`,...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const $=["Default","WithLabelAndDescription","Disabled"];export{r as Default,t as Disabled,e as WithLabelAndDescription,$ as __namedExportsOrder,Z as default};
