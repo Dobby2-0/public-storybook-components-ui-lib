6b0f85f7b07915cf863874e589302a753e22b85c
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2024-12-17 12:41:39 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@24b15ddc8ff06ee64562cae40933d903b095db15 🚀

--- components/Upload/Upload.d.ts
+++ components/Upload/Upload.d.ts
@@ -1,17 +1,25 @@
-interface UploadProps {
+interface CommonUploadProps {
+    /** label for the file input/dropzone */
+    label?: string;
+    /** description for the input/dropzone */
+    description?: string;
+    /** CSS classname for the component */
+    className?: string;
+}
+interface UploadProps extends CommonUploadProps {
     /** current value (controlled) */
     files?: File[];
     /** optionally render DropZone instead of FileInput */
     isDropZone?: boolean;
     /** error message to be displayed */
     errorMessage?: string;
-    /** description for the input/dropzone */
-    description?: string;
-    /** label for the file input/dropzone */
-    label?: string;
     /** callback to handle file selection */
     onFilesChange?: (files: File[] | File) => void;
 }
-/** Upload component */
+/**
+ *  Upload component available as single file input or file dropzone
+ *
+ *  based on `react-aria-components`
+ */
 declare const Upload: ({ errorMessage, isDropZone, ...props }: UploadProps) => import("react/jsx-runtime").JSX.Element;
 export { Upload };
