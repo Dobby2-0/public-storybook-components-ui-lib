7ed08fbfeb3da10b2a9fda0004468232cc2f7233
janaDeLetterZeron <166124646+janaDeLetterZeron@users.noreply.github.com>
2025-01-23 08:07:53 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@ba0a9b71e847d0d1cb29843d2e6d0d0c3dec8c13 🚀

---
+++ assets/IconButton.stories-CKI0T8pL.js
@@ -0,0 +1,15 @@
+import{j as r}from"./jsx-runtime-DR9Q75dM.js";import{I as x}from"./IconButton-3VsyiOdu.js";import"./index-DRjF_FHU.js";import"./index-By-mRCMU.js";import"./useFocusRing-DmDxhQCI.js";import"./index-rX-Bn4lm.js";import"./platform-SeyP_Zfo.js";import"./ButtonBase-Cv97aE-9.js";import"./Loader-Cgl1KvpE.js";import"./index-wL3GxAOQ.js";import"./Button-B8-bwU7B.js";import"./Hidden-CICGlFC4.js";import"./useFocusable-Cuxsel3J.js";import"./usePress-76wSwCmG.js";import"./openLink--SV7cA94.js";const o=({title:s,titleId:i,...q})=>r.jsxs("svg",{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 18 18",fill:"currentColor",width:"1.125rem",height:"1.125rem","aria-labelledby":i,...q,children:[s?r.jsx("title",{id:i,children:s}):null,r.jsx("path",{d:"M14.629 5.4a.65.65 0 0 1-.48-.194.65.65 0 0 1-.195-.481V4.05h-.677a.66.66 0 0 1-.482-.194.65.65 0 0 1-.195-.481q0-.287.195-.481.194-.194.482-.194h.677v-.675q0-.287.195-.481t.48-.194.478.194.193.481V2.7h.671q.285 0 .482.193a.64.64 0 0 1 .197.478q0 .285-.194.48a.65.65 0 0 1-.481.193H15.3v.675a.665.665 0 0 1-.671.681M8.1 13.05q1.35 0 2.25-.919.9-.918.9-2.231 0-1.294-.9-2.222T8.1 6.75t-2.25.928a3.1 3.1 0 0 0-.9 2.231q0 1.304.9 2.222.9.92 2.25.919m0-1.35q-.787 0-1.294-.506-.506-.507-.506-1.266t.506-1.294Q7.313 8.1 8.1 8.1t1.294.534.506 1.294-.506 1.266q-.507.506-1.294.506m-5.85 3.6q-.543 0-.947-.403A1.3 1.3 0 0 1 .9 13.95v-8.1q0-.544.403-.947.404-.403.947-.403H4.5l.938-1.256a1.4 1.4 0 0 1 .49-.403q.285-.141.597-.141h4.05q.287 0 .481.194.195.195.194.481v1.012q0 .422.295.718a.98.98 0 0 0 .717.295h.338v.337q0 .422.295.718a.98.98 0 0 0 .718.295h1.012q.287 0 .481.194.194.195.194.481v6.525q0 .544-.397.947a1.3 1.3 0 0 1-.953.403z"})]}),E={title:"Components/Button/IconButton",component:x,argTypes:{variant:{table:{defaultValue:{summary:"primary"}},control:{type:"select",options:["primary","secondary","naked","delete"]}},children:{table:{disable:!0}}}},e={args:{children:r.jsx(o,{})}},t={args:{children:r.jsx(o,{}),isDisabled:!0}},a={args:{children:r.jsx(o,{}),isLoading:!0}};var n,d,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: <AddAPhoto />
+  }
+}`,...(m=(d=e.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var l,c,p;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: <AddAPhoto />,
+    isDisabled: true
+  }
+}`,...(p=(c=t.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var u,h,g;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: <AddAPhoto />,
+    isLoading: true
+  }
+}`,...(g=(h=a.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const H=["Default","Disabled","Loading"];export{e as Default,t as Disabled,a as Loading,H as __namedExportsOrder,E as default};
