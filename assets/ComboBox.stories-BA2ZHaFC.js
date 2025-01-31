121e1df2d98c35eb9528cc12bd56f22c9075f9f8
nestorojito <116598354+nestorojito@users.noreply.github.com>
2025-01-31 19:02:05 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@88fe09969afd266a407027bde228bb1db5b4a9dc 🚀

---
+++ assets/ComboBox.stories-BA2ZHaFC.js
@@ -0,0 +1,50 @@
+import{C as b}from"./ComboBox-CO14d_ON.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./Label-C3T7MHLE.js";import"./index-By-mRCMU.js";import"./index-rX-Bn4lm.js";import"./Form-3aL0NrWF.js";import"./BulkActions-CDdV3ZcN.js";import"./Select-BJSwGkxV.js";import"./Pill-ByEbGEhU.js";import"./Close-a0dMVUhZ.js";import"./index-wL3GxAOQ.js";import"./ListBox-PrMp3-2f.js";import"./Check-f6fs0czz.js";import"./useFocusRing-C6fsr26g.js";import"./platform-SeyP_Zfo.js";import"./ListBox-pSTpXoQs.js";import"./Collection-BOD95Frq.js";import"./Hidden-BxYS409O.js";import"./DragAndDrop-DUGNgB2i.js";import"./context-gTjI1DPt.js";import"./FocusScope-fwsqHtrM.js";import"./openLink--SV7cA94.js";import"./isScrollable-BSEN4xi5.js";import"./usePress-Do_Sz9oM.js";import"./useDescription-CqZCW_mX.js";import"./useControlledState-uSQY0H5C.js";import"./Text-B95919dy.js";import"./useLabels-BwPHAfF6.js";import"./useTranslation-BZwSnrkG.js";import"./Popover-DeCJyLAk.js";import"./Dialog-CbeKxK9k.js";import"./Button-owB2lmPW.js";import"./useFocusable-DTzT9VHy.js";import"./RSPContexts-BlCrYygD.js";import"./PressResponder-EaxQ970L.js";import"./PortalProvider-DG1WN5c3.js";import"./ariaHideOutside-7Z-f0sYi.js";import"./getScrollParent-BL0kfGii.js";import"./useLocalizedStringFormatter-xOf6gwda.js";import"./VisuallyHidden-DFrqKlNW.js";import"./KeyboardArrowDown-Dio4eISq.js";import"./Info-CwC22AGT.js";import"./IconButton-BwLSw5zT.js";import"./ButtonBase-Cb9Ss4Ni.js";import"./Loader-Cgl1KvpE.js";import"./DobbyContextProvider-BRsFQSbA.js";import"./index-DlSQN3eM.js";import"./index.browser-OxPLOBIU.js";import"./Alert-Dpd35Ztu.js";import"./CheckCircle-Ciw2kuSm.js";import"./Error-BKB7cmKQ.js";import"./FileList-BM2tTchH.js";import"./Description-BZTUmzGN.js";import"./index-BSPp2CQF.js";import"./TextField-BUZvb0Y6.js";import"./FieldError-yiHUmxL5.js";import"./Form-CGpa7FVA.js";import"./useFormReset-Bw3xsAfu.js";import"./index-CrDvLK-b.js";import"./RadioGroup-Bz7UPrL4.js";import"./Tooltip-DNmfRmXA.js";import"./ArrowBack-2ExNkz_7.js";import"./Group-CpCvahxF.js";import"./useSingleSelectListState-BNfLjdOJ.js";const Ar={component:b},r={args:{placeholder:"Search animals",items:[{id:"1",label:"Aardvark"},{id:"2",label:"Cat"},{id:"3",label:"Dog"},{id:"4",label:"Kangaroo"},{id:"5",label:"Panda"},{id:"6",label:"Snake"}],children:e=>e.label}},i={args:{...r.args,label:"Favourite animal",description:"Select an animal"}},t={args:{label:"Pick an item",placeholder:"Search",isDisabled:!0,items:[{id:"1",label:"Item 1"},{id:"2",label:"Item 2"},{id:"3",label:"Item 3"}],children:e=>e.label}};var o,a,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    placeholder: "Search animals",
+    items: [{
+      id: "1",
+      label: "Aardvark"
+    }, {
+      id: "2",
+      label: "Cat"
+    }, {
+      id: "3",
+      label: "Dog"
+    }, {
+      id: "4",
+      label: "Kangaroo"
+    }, {
+      id: "5",
+      label: "Panda"
+    }, {
+      id: "6",
+      label: "Snake"
+    }],
+    // @ts-expect-error - issues with typing in storybook
+    children: item => item.label as string
+  }
+}`,...(m=(a=r.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};var n,p,l;i.parameters={...i.parameters,docs:{...(n=i.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    label: "Favourite animal",
+    description: "Select an animal"
+  }
+}`,...(l=(p=i.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var s,d,c;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: "Pick an item",
+    placeholder: "Search",
+    isDisabled: true,
+    items: [{
+      id: "1",
+      label: "Item 1"
+    }, {
+      id: "2",
+      label: "Item 2"
+    }, {
+      id: "3",
+      label: "Item 3"
+    }],
+    // @ts-expect-error - issues with typing in storybook
+    children: item => item.label as string
+  }
+}`,...(c=(d=t.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const Cr=["Default","WithLabelAndDescription","Disabled"];export{r as Default,t as Disabled,i as WithLabelAndDescription,Cr as __namedExportsOrder,Ar as default};
---
+++ assets/UseToast-DHqkJaRM.js
@@ -0,0 +1,28 @@
+import{j as t}from"./jsx-runtime-DR9Q75dM.js";import{useMDXComponents as n}from"./index-ChEI-nsM.js";import{ae as i,af as r}from"./index-C5zWF7kE.js";import"./index-DRjF_FHU.js";import"./iframe-JzJshXnO.js";import"../sb-preview/runtime.js";import"./index-rX-Bn4lm.js";import"./index-D-8MO0q_.js";import"./index-CzpEL4_8.js";import"./index-DrFu-skq.js";function o(s){const e={a:"a",code:"code",h1:"h1",p:"p",...n(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Hooks/useToast"}),`
+`,t.jsx(e.h1,{id:"usetoast",children:"UseToast"}),`
+`,t.jsxs(e.p,{children:["The ",t.jsx(e.code,{children:"useToast"}),` hook provides an easy way to trigger a toast notification.
+To use the `,t.jsx(e.code,{children:"useToast"}),", ensure that your application is wrapped in a ",t.jsx(e.a,{href:"?path=/docs/context-dobbycontextprovider--docs",children:"DobbyContextProvider"}),"."]}),`
+`,t.jsx(r,{code:`
+  const ToastComponent = () =>{
+    const toast = useToast();
+
+    return (
+
+      <Button
+        onPress={() =>
+          toast({
+            title: "Title",
+            description: "This is a test toast notification!",
+            duration,
+          })
+        }>
+        <p>Toast</p>
+      </Button>
+    )
+
+}
+`}),`
+`,t.jsxs("table",{children:[t.jsx("thead",{children:t.jsxs("tr",{children:[t.jsx("th",{children:"Prop"}),t.jsx("th",{children:"Type"}),t.jsx("th",{children:"Description"})]})}),t.jsxs("tbody",{children:[t.jsxs("tr",{children:[t.jsx("td",{children:t.jsx("code",{children:"title"})}),t.jsx("td",{children:t.jsx("code",{children:"string"})}),t.jsx("td",{children:"The title of the toast notification."})]}),t.jsxs("tr",{children:[t.jsx("td",{children:t.jsx("code",{children:"description"})}),t.jsx("td",{children:t.jsx("code",{children:"string"})}),t.jsx("td",{children:"The message displayed inside the toast."})]}),t.jsxs("tr",{children:[t.jsx("td",{children:t.jsx("code",{children:"duration"})}),t.jsx("td",{children:t.jsx("code",{children:"number"})}),t.jsx("td",{children:t.jsxs(e.p,{children:["Time in milliseconds before the toast disappears. Defaults to"," ",`
+`,t.jsx("code",{children:"5000"}),"."]})})]})]})]}),`
+`,t.jsxs(e.p,{children:["For additional properties, see the ",t.jsx(e.a,{href:"?path=/docs/components-alert--docs",children:`Alert
+documentation`}),"."]})]})}function f(s={}){const{wrapper:e}={...n(),...s.components};return e?t.jsx(e,{...s,children:t.jsx(o,{...s})}):o(s)}export{f as default};
