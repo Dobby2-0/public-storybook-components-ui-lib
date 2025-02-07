264917538d6704b9eef6e70d545586450a6b84d8
nestorojito <116598354+nestorojito@users.noreply.github.com>
2025-02-07 13:57:37 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@1c69b98337494eb815e8da048f0572969e4a92ab 🚀

--- components/Button/ButtonBase.d.ts
+++ components/Button/ButtonBase.d.ts
@@ -1,15 +1,14 @@
 import { ExcludeNull } from '../../types';
 import { VariantProps } from 'class-variance-authority';
 import { PropsWithChildren } from '../../../node_modules/react';
-import { ButtonProps as AriaButtonProps } from 'react-aria-components';
+import { ButtonProps as AriaButtonProps, PressEvent } from 'react-aria-components';
 declare const buttonBaseVariants: (props?: ({
     variant?: "primary" | "secondary" | "delete" | "ghost" | null | undefined;
 } & import('class-variance-authority/types').ClassProp) | undefined) => string;
-interface ButtonBaseProps extends Omit<AriaButtonProps, "children">, ExcludeNull<VariantProps<typeof buttonBaseVariants>>, PropsWithChildren {
-    /** Disables button and shows loading indicator */
-    isLoading?: boolean;
-    /** Whether to replace the button content while loading */
-    replaceContentWhileLoading?: boolean;
+interface ButtonBaseProps extends Omit<AriaButtonProps, "children" | "onPress">, ExcludeNull<VariantProps<typeof buttonBaseVariants>>, PropsWithChildren {
+    /** Disables button and shows loading indicator */ isLoading?: boolean;
+    /** Whether to replace the button content while loading */ replaceContentWhileLoading?: boolean;
+    /** Handler that is called when the press is released over the target */ onPress?: (e: PressEvent) => void | Promise<void>;
 }
 declare const ButtonBase: ({ className, variant, isLoading, replaceContentWhileLoading, children, ...props }: ButtonBaseProps) => import("react/jsx-runtime").JSX.Element;
 export { ButtonBase };
