7a9517c4cc4deb26391513c09e020d99d3405d50
janaDeLetterZeron <166124646+janaDeLetterZeron@users.noreply.github.com>
2025-02-11 10:48:13 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@48b076f449a0360da528106a91f9d41d098143bd 🚀

---
+++ assets/ComboBox.stories-DcbyPRUi.js
@@ -0,0 +1,61 @@
+import{C as f}from"./ComboBox-DQCxOSOK.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./IconButton-BPVQ72_G.js";import"./index-By-mRCMU.js";import"./useFocusRing-BWUU1kMe.js";import"./index-rX-Bn4lm.js";import"./platform-SeyP_Zfo.js";import"./ButtonBase-DH9QynR_.js";import"./Loader-Cgl1KvpE.js";import"./index-wL3GxAOQ.js";import"./Button-BmI6QyOV.js";import"./Hidden-3y5knXN0.js";import"./useFocusable-C701Y3LS.js";import"./usePress-8Gc9NV6T.js";import"./openLink--SV7cA94.js";import"./Label-C3T7MHLE.js";import"./ListBox-BkaNpq_2.js";import"./Check-f6fs0czz.js";import"./ListBox-DqwqOLqf.js";import"./Collection-CKo7FAVI.js";import"./DragAndDrop-SWM1ETZk.js";import"./context-CfhCFD4s.js";import"./FocusScope-Ds0qzRnM.js";import"./isScrollable-BSEN4xi5.js";import"./useDescription-BEeojJp4.js";import"./useControlledState-uSQY0H5C.js";import"./Text-CYCjNin1.js";import"./useLabels-Pvxio8V_.js";import"./useTranslation-BZwSnrkG.js";import"./Popover-CZJUG74c.js";import"./Dialog-CgYtMAXa.js";import"./RSPContexts-BlCrYygD.js";import"./PressResponder-2k5FZZvl.js";import"./PortalProvider-DG1WN5c3.js";import"./ariaHideOutside-7Z-f0sYi.js";import"./getScrollParent-BL0kfGii.js";import"./useLocalizedStringFormatter-C4j5Xp9z.js";import"./VisuallyHidden-DOCgl12y.js";import"./variants-CuMcQlp7.js";import"./Info-CwC22AGT.js";import"./KeyboardArrowDown-Dio4eISq.js";import"./FieldError-C2OjUulo.js";import"./Form-BPFCzTC-.js";import"./Group-BCR4DJoW.js";import"./Input-CXtscATe.js";import"./useFormReset-29dva5OZ.js";import"./useSingleSelectListState-CvYOCSqh.js";const hr={component:f},r={args:{placeholder:"Search animals",items:[{id:"1",label:"Aardvark"},{id:"2",label:"Cat"},{id:"3",label:"Dog"},{id:"4",label:"Kangaroo"},{id:"5",label:"Panda"},{id:"6",label:"Snake"}],children:o=>o.label,allowsEmptyCollection:!0}},e={args:{...r.args,label:"Favourite animal",description:"Select an animal"}},a={args:{label:"Pick an item",placeholder:"Search",isDisabled:!0,items:[{id:"1",label:"Item 1"},{id:"2",label:"Item 2"},{id:"3",label:"Item 3"}],children:o=>o.label}},t={args:{...r.args,loading:!0}},i={args:{...r.args,items:[]}};var n,s,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    placeholder: "Search animals",
+    items: [{
+      id: "1",
+      label: "Aardvark"
+    }, {
+      id: "2",
+      label: "Cat"
+    }, {
+      id: "3",
+      label: "Dog"
+    }, {
+      id: "4",
+      label: "Kangaroo"
+    }, {
+      id: "5",
+      label: "Panda"
+    }, {
+      id: "6",
+      label: "Snake"
+    }],
+    // @ts-expect-error - issues with typing in storybook
+    children: item => item.label as string,
+    allowsEmptyCollection: true
+  }
+}`,...(m=(s=r.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};var l,p,c;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    label: "Favourite animal",
+    description: "Select an animal"
+  }
+}`,...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var d,b,g;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: "Pick an item",
+    placeholder: "Search",
+    isDisabled: true,
+    items: [{
+      id: "1",
+      label: "Item 1"
+    }, {
+      id: "2",
+      label: "Item 2"
+    }, {
+      id: "3",
+      label: "Item 3"
+    }],
+    // @ts-expect-error - issues with typing in storybook
+    children: item => item.label as string
+  }
+}`,...(g=(b=a.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var u,h,D;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    loading: true
+  }
+}`,...(D=(h=t.parameters)==null?void 0:h.docs)==null?void 0:D.source}}};var S,k,y;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    items: []
+  }
+}`,...(y=(k=i.parameters)==null?void 0:k.docs)==null?void 0:y.source}}};const Dr=["Default","WithLabelAndDescription","Disabled","Loading","emptyList"];export{r as Default,a as Disabled,t as Loading,e as WithLabelAndDescription,Dr as __namedExportsOrder,hr as default,i as emptyList};
---
+++ assets/ListBox.stories-CALB3w_n.js
@@ -0,0 +1,109 @@
+import{L}from"./ListBox-BkaNpq_2.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./Loader-Cgl1KvpE.js";import"./index-By-mRCMU.js";import"./index-wL3GxAOQ.js";import"./Check-f6fs0czz.js";import"./useFocusRing-BWUU1kMe.js";import"./index-rX-Bn4lm.js";import"./platform-SeyP_Zfo.js";import"./ListBox-DqwqOLqf.js";import"./Collection-CKo7FAVI.js";import"./Hidden-3y5knXN0.js";import"./DragAndDrop-SWM1ETZk.js";import"./context-CfhCFD4s.js";import"./FocusScope-Ds0qzRnM.js";import"./openLink--SV7cA94.js";import"./isScrollable-BSEN4xi5.js";import"./usePress-8Gc9NV6T.js";import"./useDescription-BEeojJp4.js";import"./useControlledState-uSQY0H5C.js";import"./Text-CYCjNin1.js";import"./useLabels-Pvxio8V_.js";import"./useTranslation-BZwSnrkG.js";const Y={component:L},t={args:{items:[{id:"1",label:"Item 1"},{id:"2",label:"Item 2"},{id:"3",label:"Item 3"}],labelResolver:e=>e.label}},r={args:{...t.args,selectionMode:"single"}},n={args:{...t.args,selectionMode:"multiple"}},l={args:{selectionMode:"single",items:[{id:"1",label:"Item 1"},{id:"2",label:"Item 2"},{id:"3",label:"Item 3"}],children:(e,v)=>(v.isDisabled=e.id==="3",e.label)}},s={args:{items:[],renderEmptyState:()=>"No results found."}},a={args:{className:"max-h-64",items:[{id:"1",label:"Item 1"},{id:"2",label:"Item 2"},{id:"3",label:"Item 3"},{id:"4",label:"Item 4"},{id:"5",label:"Item 5"},{id:"6",label:"Item 6"},{id:"7",label:"Item 7"},{id:"8",label:"Item 8"},{id:"9",label:"Item 9"},{id:"10",label:"Item 10"},{id:"11",label:"Item 11"},{id:"12",label:"Item 12"},{id:"13",label:"Item 13"},{id:"14",label:"Item 14"},{id:"15",label:"Item 15"}],labelResolver:e=>e.label,onLoadMore:()=>(console.warn("loadMore"),new Promise(e=>{setTimeout(()=>e(!1),1e3)}))}};var i,o,m;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      id: "1",
+      label: "Item 1"
+    }, {
+      id: "2",
+      label: "Item 2"
+    }, {
+      id: "3",
+      label: "Item 3"
+    }],
+    // @ts-expect-error - issues with typing in storybook
+    labelResolver: item => item.label as string
+  }
+}`,...(m=(o=t.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};var d,p,c;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    selectionMode: "single"
+  }
+}`,...(c=(p=r.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var b,I,u;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    selectionMode: "multiple"
+  }
+}`,...(u=(I=n.parameters)==null?void 0:I.docs)==null?void 0:u.source}}};var g,S,y;l.parameters={...l.parameters,docs:{...(g=l.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    selectionMode: "single",
+    items: [{
+      id: "1",
+      label: "Item 1"
+    }, {
+      id: "2",
+      label: "Item 2"
+    }, {
+      id: "3",
+      label: "Item 3"
+    }],
+    children: (item, extraProps) => {
+      // @ts-expect-error - issues with typing in storybook
+      extraProps.isDisabled = item.id === "3";
+      // @ts-expect-error - issues with typing in storybook
+      return item.label as string;
+    }
+  }
+}`,...(y=(S=l.parameters)==null?void 0:S.docs)==null?void 0:y.source}}};var M,x,f;s.parameters={...s.parameters,docs:{...(M=s.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    items: [],
+    renderEmptyState: () => "No results found."
+  }
+}`,...(f=(x=s.parameters)==null?void 0:x.docs)==null?void 0:f.source}}};var h,w,D;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    className: "max-h-64",
+    items: [{
+      id: "1",
+      label: "Item 1"
+    }, {
+      id: "2",
+      label: "Item 2"
+    }, {
+      id: "3",
+      label: "Item 3"
+    }, {
+      id: "4",
+      label: "Item 4"
+    }, {
+      id: "5",
+      label: "Item 5"
+    }, {
+      id: "6",
+      label: "Item 6"
+    }, {
+      id: "7",
+      label: "Item 7"
+    }, {
+      id: "8",
+      label: "Item 8"
+    }, {
+      id: "9",
+      label: "Item 9"
+    }, {
+      id: "10",
+      label: "Item 10"
+    }, {
+      id: "11",
+      label: "Item 11"
+    }, {
+      id: "12",
+      label: "Item 12"
+    }, {
+      id: "13",
+      label: "Item 13"
+    }, {
+      id: "14",
+      label: "Item 14"
+    }, {
+      id: "15",
+      label: "Item 15"
+    }],
+    // @ts-expect-error - issues with typing in storybook
+    labelResolver: item => item.label as string,
+    onLoadMore: () => {
+      console.warn("loadMore");
+      return new Promise(resolve => {
+        setTimeout(() => resolve(false), 1000);
+      });
+    }
+  }
+}`,...(D=(w=a.parameters)==null?void 0:w.docs)==null?void 0:D.source}}};const Z=["Default","SingleSelection","MultipleSelection","DisabledItem","EmptyList","Scroll"];export{t as Default,l as DisabledItem,s as EmptyList,n as MultipleSelection,a as Scroll,r as SingleSelection,Z as __namedExportsOrder,Y as default};
