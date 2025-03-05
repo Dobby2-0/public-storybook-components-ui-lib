47744ecb91cb1e8b02d4fc8fbbca0b104ddf6746
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2025-03-05 09:16:13 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@6dfd325b71a248df111682a3a0c01b2b037645ca 🚀

--- utils/date.d.ts
+++ utils/date.d.ts
@@ -1,3 +1,4 @@
+export declare const formatDateString: (format: string, date?: string) => string;
 export declare const formatDate: (date?: string) => string;
 export declare const formatDateTime: (date?: string) => string;
 export declare const formatTime: (date?: string) => string;
