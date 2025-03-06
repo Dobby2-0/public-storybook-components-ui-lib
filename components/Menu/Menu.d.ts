310e6ce2c13072a87594423721b6e6ae05f29a91
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2025-03-06 08:10:44 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@28ba22a49baa4dca177b199e51aa4d8692135491 🚀

--- components/Menu/Menu.d.ts
+++ components/Menu/Menu.d.ts
@@ -1,14 +1,20 @@
 import { ReactNode } from '../../../node_modules/react';
 import { MenuItemProps as AriaMenuItemProps, MenuProps as AriaMenuProps, MenuTriggerProps as AriaMenuTriggerProps, SeparatorProps as AriaSeparatorProps, PopoverProps } from 'react-aria-components';
-interface MenuBaseProps {
+interface ClassNameObject {
+    base?: string;
+    popover?: string;
+}
+interface MenuBaseProps<T extends object> extends Omit<AriaMenuProps<T>, "items" | "className"> {
     /** The placement of the menu with respect to its trigger element. */
     placement?: PopoverProps["placement"];
     /** The additional offset applied along the main axis between the element and its anchor element. */
     offset?: PopoverProps["offset"];
+    /**  */
+    className?: string | ClassNameObject;
 }
 declare const MenuItem: ({ children, className, ...props }: AriaMenuItemProps) => import("react/jsx-runtime").JSX.Element;
 declare const MenuSeparator: ({ className, ...props }: AriaSeparatorProps) => import("react/jsx-runtime").JSX.Element;
-interface MenuProps<T extends object> extends Omit<AriaMenuProps<T>, "items">, Omit<AriaMenuTriggerProps, "children">, MenuBaseProps {
+interface MenuProps<T extends object> extends Omit<AriaMenuProps<T>, "items" | "className">, Omit<AriaMenuTriggerProps, "children">, MenuBaseProps<T> {
     /** Array of items used to build the hierarchy */
     items: (MenuItem | MenuSeparator)[];
     /** Element to be shown as menu trigger */
