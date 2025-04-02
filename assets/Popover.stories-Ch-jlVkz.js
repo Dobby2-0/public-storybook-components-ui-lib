de3aab2d7315c915ec2c9ffc0ffa68ac71c007d0
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2025-04-02 08:31:23 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@43b4c2b6c72b637e9f32bdea5b4fd371f1a6fe66 🚀

---
+++ assets/Popover.stories-Ch-jlVkz.js
@@ -0,0 +1,16 @@
+import{j as g}from"./jsx-runtime-F1iDwNaf.js";import{B as d}from"./Button-DCebkjR3.js";import{a as l}from"./Popover-BLgbbJC7.js";import"./index-YTOF1_EJ.js";import"./index-DzfFlqCY.js";import"./index-UoR2Am_h.js";import"./useFocusRing-BCcVRV1W.js";import"./platform-DO9ne4TU.js";import"./ButtonBase-D_bDrwCz.js";import"./Loader-xB9gCnz2.js";import"./Button-aud4VQvi.js";import"./Hidden-D3qdwGc7.js";import"./index-Cjh3zZyl.js";import"./index-BhTLYoCu.js";import"./useFocusable-CYgatKa4.js";import"./usePress-Qy4Tuk_n.js";import"./openLink-8mltwmIb.js";import"./Dialog-CG1btdUR.js";import"./RSPContexts-uj8S1pO4.js";import"./Collection-D72pwjyt.js";import"./Separator-Bia4ZK9j.js";import"./useDescription-CQdQCKlk.js";import"./FocusScope-Bq1MVHph.js";import"./context-B1-8vjow.js";import"./useControlledState-BLnGQsC8.js";import"./Text-CUzL_Ejp.js";import"./PressResponder-BL3btCGW.js";import"./useLocalizedStringFormatter-BIwj9O_3.js";import"./PortalProvider-D04PMIEq.js";import"./ariaHideOutside-7Z-f0sYi.js";import"./getScrollParent-CYkSmvrp.js";import"./useLabels-Dzu6Vces.js";import"./VisuallyHidden-i3w_VAQN.js";const U={component:l,argTypes:{}},r={args:{children:g.jsx(d,{children:"Open popover"}),content:"Popover content"}},o={args:{...r.args,overlayArrow:!0}},t={args:{...r.args,placement:"right"}};var e,p,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    children: <Button>Open popover</Button>,
+    content: "Popover content"
+  }
+}`,...(a=(p=r.parameters)==null?void 0:p.docs)==null?void 0:a.source}}};var m,i,s;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    overlayArrow: true
+  }
+}`,...(s=(i=o.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var n,c,u;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    placement: "right"
+  }
+}`,...(u=(c=t.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};const V=["Default","WithArrow","PositionRight"];export{r as Default,t as PositionRight,o as WithArrow,V as __namedExportsOrder,U as default};
