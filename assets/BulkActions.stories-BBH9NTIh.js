e74e14bf5c75eb913b91bdfa868c28536b987a43
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2025-04-08 09:57:50 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@c2cf304b6704134e8e29d20400f0ab275987c2be 🚀

---
+++ assets/BulkActions.stories-BBH9NTIh.js
@@ -0,0 +1,16 @@
+import{B as n}from"./BulkActions-CcznFM3-.js";import"./jsx-runtime-F1iDwNaf.js";import"./index-YTOF1_EJ.js";import"./Select-TkQ41pe9.js";import"./Pill-CnNhOt2h.js";import"./index-DzfFlqCY.js";import"./Close-CqA61BEN.js";import"./index-UoR2Am_h.js";import"./SelectBase-BVx5R5Ye.js";import"./Label-B2LZGk7B.js";import"./index-DFyT2So3.js";import"./index-Cjh3zZyl.js";import"./index-BhTLYoCu.js";import"./ListBox-CqCy1g2Z.js";import"./Loader-xB9gCnz2.js";import"./Check-d3BnqXpp.js";import"./useFocusRing-C4BMZp44.js";import"./platform-DO9ne4TU.js";import"./ListBox-CwX2D75j.js";import"./Separator-CbEL6sJt.js";import"./openLink-8mltwmIb.js";import"./usePress-vd6OoI-D.js";import"./useDescription-Bhr3VKQH.js";import"./context-gkfeJ4AL.js";import"./useControlledState-BLnGQsC8.js";import"./Hidden-BOYwTkK6.js";import"./DragAndDrop-B_G89oNp.js";import"./Text-DV3bYAm4.js";import"./useLabel-CffM8sE5.js";import"./useLabels-ORoINcrz.js";import"./useTranslation-Dw3DpAYw.js";import"./Popover-DAK_5kFI.js";import"./Dialog-MoVWtpFC.js";import"./Button-BM5vVtVI.js";import"./useFocusable-DxTGOERD.js";import"./RSPContexts-uj8S1pO4.js";import"./PressResponder-BU6yU8x-.js";import"./ariaHideOutside-DUj27I7Q.js";import"./PortalProvider-D04PMIEq.js";import"./getScrollParent-ByVXNY_H.js";import"./VisuallyHidden-kxgC2G6T.js";import"./variants-DIvSP2G7.js";import"./Info-Cvx1OggM.js";import"./KeyboardArrowDown-C_KWurVb.js";import"./index-u5j6GQhH.js";const X={component:n},o={args:{count:1,actions:[{id:"action-1",label:"Action 1",action:()=>console.log("Action 1")},{id:"action-2",label:"Action 2",action:()=>console.log("Action 2")}]}};var t,i,r;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    count: 1,
+    actions: [{
+      id: "action-1",
+      label: "Action 1",
+      // eslint-disable-next-line no-console
+      action: () => console.log("Action 1")
+    }, {
+      id: "action-2",
+      label: "Action 2",
+      // eslint-disable-next-line no-console
+      action: () => console.log("Action 2")
+    }]
+  }
+}`,...(r=(i=o.parameters)==null?void 0:i.docs)==null?void 0:r.source}}};const Y=["Default"];export{o as Default,Y as __namedExportsOrder,X as default};
