1f3efe1c161e5376cdd0f1f79be4350c35d4eba6
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2025-06-24 12:54:52 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@73f8301f7c10fcbb0a463cbf44a127fd8c54a2a0 🚀

---
+++ assets/FileList.stories-B2BQS-AJ.js
@@ -0,0 +1,90 @@
+import{j as l}from"./jsx-runtime-Cf8x2fCZ.js";import{B as P}from"./Button-sCQSYoU7.js";import{F as W}from"./FileList-EVGzbH5_.js";import{q as C}from"./Error-DDcq_WG-.js";import"./index-yBjzXJbu.js";import"./index-D8pzxCuu.js";import"./index-D35RSF8M.js";import"./useFocusRing-BbqFvpEd.js";import"./index-tvICUrOf.js";import"./platform-DO9ne4TU.js";import"./ButtonBase-CbJHwgyj.js";import"./Loader-BsPrQ8_e.js";import"./Tooltip-BOKqtUjG.js";import"./index-BgHXMf9R.js";import"./Button-B72osXTA.js";import"./Separator-F-TqXpng.js";import"./usePress-BklFyQpc.js";import"./openLink-Btrk4R3c.js";import"./index-CW62Yr2X.js";import"./index-fNjTmf9T.js";import"./FocusScope-DTcBUnqK.js";import"./Hidden-975QGLs-.js";import"./RSPContexts-B1Ggupi0.js";import"./Text-B6XONKPt.js";import"./VisuallyHidden-C1QUg0KJ.js";import"./PortalProvider-ChirZUt_.js";import"./IconButton-BV6bN9b2.js";import"./FileIcon-BX_A-QGh.js";import"./Description-Xf8xniLu.js";import"./Pill-D52yXNx1.js";import"./Close-Lw3CM0IZ.js";import"./index.browser-OxPLOBIU.js";import"./useTranslation-rxOr_aTQ.js";const me={component:W,argTypes:{actionButtons:{control:{type:"boolean"}}}},t={args:{files:[{id:"1",name:"file1.txt",size:100,type:"text",url:"https://example.com/file1.txt"},{id:"2",name:"image1.png",size:541,type:"image/png",url:"https://example.com/image1.png"}]}},r={args:{...t.args,actionButtons:{view:(e,n)=>{console.log(`Viewing ${e.name}`),n(e.url)},download:(e,n)=>{console.log(`Downloading ${e.name}`),n(e.url,e.name)},delete:e=>console.log(`Deleting ${e.name}`)}}},o={args:{...t.args,actionButtons:(e,n)=>l.jsxs("div",{className:"flex items-center gap-2",children:["Custom actions for ",e.name,l.jsx(P,{variant:"delete",onPress:()=>n.delete(e.id),children:"Delete"})]})}},i={args:{files:[{id:"1",name:"file1.txt",size:100,type:"text",url:"https://example.com/file1.txt",loading:new Promise(()=>{})}]}},a={args:{files:[{id:"1",name:"file1.txt",size:100,type:"text",url:"https://example.com/file1.txt",loading:Promise.reject(new Error("Failed to upload file"))},{id:"2",name:"image1.png",size:541,type:"image/png",url:"https://example.com/image1.png",loading:Promise.reject(new Error)}]}},s={args:{files:[{id:"1",name:"file1.txt",size:100,type:"text",url:"https://example.com/file1.txt",loading:Promise.reject(new Error("Failed to upload file"))}],onFileLoadingError:({error:e})=>l.jsxs("div",{className:"flex items-center gap-1.5 text-amber-700",children:[l.jsx(C,{})," Custom error: ",e]})}};var m,p,c;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    files: [{
+      id: "1",
+      name: "file1.txt",
+      size: 100,
+      type: "text",
+      url: "https://example.com/file1.txt"
+    }, {
+      id: "2",
+      name: "image1.png",
+      size: 541,
+      type: "image/png",
+      url: "https://example.com/image1.png"
+    }]
+  }
+}`,...(c=(p=t.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var d,g,u;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    actionButtons: {
+      view: (file, original) => {
+        // eslint-disable-next-line no-console
+        console.log(\`Viewing \${file.name}\`);
+        original(file.url);
+      },
+      download: (file, original) => {
+        // eslint-disable-next-line no-console
+        console.log(\`Downloading \${file.name}\`);
+        original(file.url, file.name);
+      },
+      // eslint-disable-next-line no-console
+      delete: file => console.log(\`Deleting \${file.name}\`)
+    }
+  }
+}`,...(u=(g=r.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var f,x,h;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    actionButtons: (file, defaultActionHandlers) => <div className="flex items-center gap-2">
+        Custom actions for {file.name}
+        <Button variant="delete" onPress={() => defaultActionHandlers.delete(file.id)}>
+          Delete
+        </Button>
+      </div>
+  }
+}`,...(h=(x=o.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var w,E,y;i.parameters={...i.parameters,docs:{...(w=i.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    files: [{
+      id: "1",
+      name: "file1.txt",
+      size: 100,
+      type: "text",
+      url: "https://example.com/file1.txt",
+      loading: new Promise(() => undefined)
+    }]
+  }
+}`,...(y=(E=i.parameters)==null?void 0:E.docs)==null?void 0:y.source}}};var v,B,j;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    files: [{
+      id: "1",
+      name: "file1.txt",
+      size: 100,
+      type: "text",
+      url: "https://example.com/file1.txt",
+      loading: Promise.reject(new Error("Failed to upload file"))
+    }, {
+      id: "2",
+      name: "image1.png",
+      size: 541,
+      type: "image/png",
+      url: "https://example.com/image1.png",
+      loading: Promise.reject(new Error())
+    }]
+  }
+}`,...(j=(B=a.parameters)==null?void 0:B.docs)==null?void 0:j.source}}};var z,D,F;s.parameters={...s.parameters,docs:{...(z=s.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    files: [{
+      id: "1",
+      name: "file1.txt",
+      size: 100,
+      type: "text",
+      url: "https://example.com/file1.txt",
+      loading: Promise.reject(new Error("Failed to upload file"))
+    }],
+    onFileLoadingError: ({
+      error
+    }) => <div className="flex items-center gap-1.5 text-amber-700">
+        <ErrorIcon /> Custom error: {error}
+      </div>
+  }
+}`,...(F=(D=s.parameters)==null?void 0:D.docs)==null?void 0:F.source}}};const pe=["Default","WithActionButtonsConfig","WithActionButtonsOverride","WithLoadingFile","WithLoadingError","WithCustomErrorRenderer"];export{t as Default,r as WithActionButtonsConfig,o as WithActionButtonsOverride,s as WithCustomErrorRenderer,a as WithLoadingError,i as WithLoadingFile,pe as __namedExportsOrder,me as default};
