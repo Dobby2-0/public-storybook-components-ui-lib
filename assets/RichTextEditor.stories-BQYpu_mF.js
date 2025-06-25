b230092c9d4c65ce12a138c1a685cb93b47ce1e9
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2025-06-25 07:53:57 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@1118f5092a2b6a423f1c83e87046a0a5d3904b85 🚀

---
+++ assets/RichTextEditor.stories-BQYpu_mF.js
@@ -0,0 +1,35 @@
+import{R as x}from"./RichTextEditor-CqX_mqsu.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./IconButton-CqidJaOF.js";import"./index-D8pzxCuu.js";import"./useFocusRing-BbqFvpEd.js";import"./index-tvICUrOf.js";import"./platform-DO9ne4TU.js";import"./ButtonBase-bTkKKm01.js";import"./Loader-BsPrQ8_e.js";import"./index-D35RSF8M.js";import"./Tooltip-DyfBqGJt.js";import"./index-BOg9ZCtA.js";import"./Button-B72osXTA.js";import"./Separator-F-TqXpng.js";import"./usePress-BklFyQpc.js";import"./openLink-Btrk4R3c.js";import"./index-CW62Yr2X.js";import"./index-fNjTmf9T.js";import"./FocusScope-DTcBUnqK.js";import"./Hidden-975QGLs-.js";import"./RSPContexts-B1Ggupi0.js";import"./Text-B6XONKPt.js";import"./VisuallyHidden-C1QUg0KJ.js";import"./PortalProvider-ChirZUt_.js";import"./Button-BqXPgpwz.js";import"./useTranslation-rxOr_aTQ.js";import"./Menu-cQWXa35E.js";import"./Check-CsX-RmMH.js";import"./ChevronForward-CkT6jMu6.js";import"./KeyboardArrowDown-3e0Y-526.js";import"./Toolbar-RasgINYC.js";const U={component:x},e={args:{className:"min-w-[400px]"}},r={args:{...e.args,toolbarOptions:{tagMenu:[{id:"1",label:"Building name",textValue:"{{building.name}}"},{id:"2",label:"Meeting title",textValue:"{{meeting.title}}"},{id:"3",label:"President",textValue:"{{president}}"}]}}},t={args:{...e.args,value:"This is a disabled editor",disabled:!0}},a={args:{className:"min-w-[400px] max-h-[250px]",resizable:!1}};var i,s,o;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    className: "min-w-[400px]"
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var n,m,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    toolbarOptions: {
+      tagMenu: [{
+        id: "1",
+        label: "Building name",
+        textValue: "{{building.name}}"
+      }, {
+        id: "2",
+        label: "Meeting title",
+        textValue: "{{meeting.title}}"
+      }, {
+        id: "3",
+        label: "President",
+        textValue: "{{president}}"
+      }]
+    }
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var l,d,c;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    value: "This is a disabled editor",
+    disabled: true
+  }
+}`,...(c=(d=t.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var u,g,b;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    className: "min-w-[400px] max-h-[250px]",
+    resizable: false
+  }
+}`,...(b=(g=a.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};const X=["Default","WithTagSelect","Disabled","NonResizable"];export{e as Default,t as Disabled,a as NonResizable,r as WithTagSelect,X as __namedExportsOrder,U as default};
