49e07225d2ea5cd7b45c06f251e0e944abc47745
JVerv <jens.vervaeren@dobby.be>
2026-07-01 09:28:50 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@6147e0dec6d8f5fe7594e3a35507f0a492c073e8 🚀

---
+++ assets/PDFViewerModal.stories-YUBF2pVX.js
@@ -0,0 +1,7 @@
+import{c as e,i as t}from"./preload-helper-usAeo7Bx.js";import{t as n}from"./react-BfNQUoGZ.js";import{t as r}from"./jsx-runtime-O9QVJvLM.js";import{n as i,t as a}from"./Button-j8TcjU4G.js";import{n as o,t as s}from"./PDFViewerModal-rbB9E045.js";var c,l,u,d,f,p;t((()=>{o(),c=e(n(),1),i(),l=r(),u={component:s,title:`Components/PDFViewer/Modal`},d=e=>{let[t,n]=(0,c.useState)(!1);return(0,l.jsxs)(l.Fragment,{children:[(0,l.jsx)(a,{onPress:()=>n(!0),children:`Preview PDF`}),(0,l.jsx)(s,{isOpen:t,onOpenChange:n,...e})]})},f={render:e=>(0,l.jsx)(d,{...e}),args:{url:`./src/assets/Dobby-Flyer.pdf`,title:`PDF preview`}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: args => <PDFViewerModalDemo {...args} />,
+  args: {
+    url: "./src/assets/Dobby-Flyer.pdf",
+    title: "PDF preview"
+  }
+}`,...f.parameters?.docs?.source}}},p=[`Default`]}))();export{f as Default,p as __namedExportsOrder,u as default};
