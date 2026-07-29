5def075f07df96fd48746f2bebe422b84a68c118
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2026-07-29 11:19:28 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@5257a98cf1d0bba341167328834d576448b20ccd 🚀

--- assets/UseToast.stories-ClN9ZKPo.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{h as t,m as n}from"./ButtonBase-DcNsaweK.js";import{t as r}from"./jsx-runtime-O9QVJvLM.js";import{a as i,o as a}from"./iframe-CycvlnSD.js";import{n as o,t as s}from"./Button-Bv6Mt9zc.js";var c,l,u,d,f,p=e((()=>{n(),o(),a(),c=r(),l={component:i,title:`Hooks/useToast`,tags:[`!autodocs`,`!dev`]},u=({id:e,duration:n})=>{let r=t(),i=`Toast Title `+e;return(0,c.jsx)(s,{onPress:()=>r({title:i,children:`This is a test toast notification!`,onClose:()=>console.warn(`Toast closed`),duration:n}),children:(0,c.jsxs)(`p`,{children:[`Toast `,e]})})},d={render:()=>(0,c.jsx)(`div`,{className:`h-[200px] w-[500px]`,children:(0,c.jsx)(u,{id:`1`,duration:5e3})})},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
-  render: () => {
-    return <div className="h-[200px] w-[500px]">
-        <ToastButton id="1" duration={5000} />
-      </div>;
-  }
-}`,...d.parameters?.docs?.source}}},f=[`Default`]}));p();export{d as Default,f as __namedExportsOrder,l as default,p as t};
