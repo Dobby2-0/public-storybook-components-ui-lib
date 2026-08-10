c06ba57262bffedea69851e51d56334d2d078496
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2026-08-10 07:48:31 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@ade8b26d54870d1a2952786a2aae7c7946d0ca43 🚀

---
+++ assets/IconButton.stories-kgHvsKzt.js
@@ -0,0 +1,23 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-O9QVJvLM.js";import{gn as n,t as r}from"./main-DS6peC0q.js";import{n as i,t as a}from"./IconButton-D4R2FJem.js";var o,s,c,l,u,d,f;e((()=>{r(),i(),o=t(),s={title:`Components/Button/IconButton`,component:a,argTypes:{variant:{table:{defaultValue:{summary:`primary`}},control:{type:`select`,options:[`primary`,`secondary`,`naked`,`delete`]}},children:{table:{disable:!0}}}},c={args:{children:(0,o.jsx)(n,{})}},l={args:{...c.args,isDisabled:!0}},u={args:{...c.args,isLoading:!0}},d={args:{...c.args,tooltip:`Tooltip`,tooltipProps:{placement:`bottom`}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <AddAPhoto />
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    isDisabled: true
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    isLoading: true
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    tooltip: "Tooltip",
+    tooltipProps: {
+      placement: "bottom"
+    }
+  }
+}`,...d.parameters?.docs?.source}}},f=[`Default`,`Disabled`,`Loading`,`WithTooltip`]}))();export{c as Default,l as Disabled,u as Loading,d as WithTooltip,f as __namedExportsOrder,s as default};
