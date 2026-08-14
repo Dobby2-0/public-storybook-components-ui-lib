f595f68f12a47c0a2865e1fb00ba9b07d6f15bb4
Tibbo-Van-Leemput <Tibbo.vanleemput@dobby.be>
2026-08-14 11:14:48 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@107e301d287e48eb8a89a3f2c1df2dd1daca6bc3 🚀

---
+++ assets/PDFViewer.stories-Bjp2nK-B.js
@@ -0,0 +1,17 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-O9QVJvLM.js";import{n,t as r}from"./PDFViewer-ClacmRfG.js";import{n as i,t as a}from"./Button-BFEZeOdL.js";var o,s,c,l,u,d;e((()=>{i(),n(),o=t(),s={component:r,title:`Components/PDFViewer/Viewer`},c={args:{url:`./src/assets/Dobby-Flyer.pdf`,className:`w-[90vw] h-[75vh]`}},l={args:{...c.args,enableDownload:!0,fileName:`Dobby-Flyer.pdf`}},u={args:{...c.args,customControls:(0,o.jsx)(a,{children:`Do something`})}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    url: "./src/assets/Dobby-Flyer.pdf",
+    className: "w-[90vw] h-[75vh]"
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    enableDownload: true,
+    fileName: "Dobby-Flyer.pdf"
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    customControls: <Button>Do something</Button>
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Default`,`Download`,`WithCustomControls`]}))();export{c as Default,l as Download,u as WithCustomControls,d as __namedExportsOrder,s as default};
