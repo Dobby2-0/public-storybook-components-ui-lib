45eafa406274b9fce6b7114ced9f302fa02ffed2
jarno-zeron <119666164+jarno-zeron@users.noreply.github.com>
2026-08-26 10:31:54 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@649c88c1b3a624806abfbac2fe37b1b036a33c25 🚀

---
+++ assets/Pagination.stories-DATpHpUB.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t,t as n}from"./Pagination-D-Qgq4Uv.js";var r,i,a;e((()=>{t(),r={component:n},i={args:{defaultCurrentPage:1,totalPages:5}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultCurrentPage: 1,
+    totalPages: 5
+  }
+}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
---
+++ assets/mdx-react-shim-D5Qxe4S8.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{b as t}from"./blocks-BPf-gc6H.js";var n=e((()=>{t()}));export{n as t};
