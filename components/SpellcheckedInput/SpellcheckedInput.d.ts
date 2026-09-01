7dce45510bd2fc8459e2cbb265dcf5ba78093fe3
JVerv <jens.vervaeren@dobby.be>
2026-09-01 09:12:21 +0000

    Deploying to gh-pages from @ Dobby2-0/components-ui-lib@2ada0cb7a083f64cd5279bc029bc2eb18ab08d8e 🚀

--- components/SpellcheckedInput/SpellcheckedInput.d.ts
+++ components/SpellcheckedInput/SpellcheckedInput.d.ts
@@ -3,12 +3,16 @@ interface SpellcheckedInputProps {
     type?: "text" | "textarea";
     /** The value of the input */
     value?: string;
+    /** The placeholder text shown when the input is empty */
+    placeholder?: string;
     /** Whether the input should be disabled */
     disabled?: boolean;
     /** Handler called when the value changes */
     onChange?: (value: string) => void;
+    /** Handler called when the input loses focus */
+    onBlur?: () => void;
     /** The className of the editor */
     className?: string;
 }
-declare const SpellcheckedInput: ({ type, value, disabled, onChange, className, }: SpellcheckedInputProps) => import("react/jsx-runtime").JSX.Element | undefined;
+declare const SpellcheckedInput: ({ type, value, placeholder, disabled, onChange, onBlur, className, }: SpellcheckedInputProps) => import("react/jsx-runtime").JSX.Element | undefined;
 export { SpellcheckedInput };
